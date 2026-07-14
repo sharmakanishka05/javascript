be558f545b593041123261acdd4b30eb71361ac4
Kanishka Sharma <kanishkasharma644@gmail.com>
2026-07-14 13:29:35 +0000

    basic

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,15 @@
+const accountId=122445
+var accountEmail="kanishka@1123"
+/*
+var is functional scope(prefer not to use var because of issue in 
+block scope and finctional scope)
+and let is block scope
+*/
+let accountPassword="123456"
+let accountState;
+accountCity="jaipur"
+accountEmail="k@123google.com"
+//accountId=456343
+accountPassword="kanishka123"
+accountCity="Bengaluru"
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict";//treat all js code as newer version
+//alert("hello")//we are using node js not browser
+/*
+number=2 t power 53
+bigint=use for large number(eg-trading,stock)
+string=""
+boolean=true/false
+null=standalone value
+undefined=value is not assign
+symbol=unique
+*/
+//object
+console.log(typeof null)
+
+
