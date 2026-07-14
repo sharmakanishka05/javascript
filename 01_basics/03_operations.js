cb105b6d7d1b9ffb5c310faf676e7e1d3d055679
Kanishka Sharma <kanishkasharma644@gmail.com>
2026-07-14 14:36:56 +0000

    conversion of datatypes

---
+++ 01_basics/03_operations.js
@@ -0,0 +1,26 @@
+let score="kanishka"
+console.log(typeof score)
+console.log(typeof(score))
+let valueInNumber=Number(score)
+console.log(typeof valueInNUmber)
+console.log(valueInNumber)//output -> nan (not a number)
+/*
+"33"=33
+"33abn"=NaN
+true=1,false=0
+
+*/
+let isLoggedIn=67
+let booleanIsLoggein=Boolean(isLoggedIn)
+console.log(booleanIsLoggein)
+/*
+1= true,0=false
+""=false
+"kanishka"=true
+67=true
+*/
+
+let number=33
+let stringNumber=String(number)
+console.log(stringNumber)
+console.log(typeof stringNumber)
