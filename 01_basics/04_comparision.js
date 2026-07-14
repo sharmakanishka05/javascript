00bd499b9587e1ce95fcc08a148ec45d39a35334
Kanishka Sharma <kanishkasharma644@gmail.com>
2026-07-14 15:09:09 +0000

    comparision

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,10 @@
+// console.log("2">1)
+// console.log("02">1)
+console.log(null>0)//sometime null convert into NaN or 0
+console.log(null==0)
+console.log(null>=0)//true
+console.log(null<=0)//true
+console.log(undefined==0)//false
+
+//===
+console.log("2"===2);
